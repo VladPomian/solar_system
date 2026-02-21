d7ec19aa889128684aea8fea604b32b92a865a65
VladPomian <34polbof@gmail.com>
2026-02-21 16:17:46 +0200

    dev версия 1.0.0

--- lib/features/overlay/settings/dialog/app_version_dialog.dart
+++ lib/features/overlay/settings/dialog/app_version_dialog.dart
@@ -10,13 +10,13 @@ void showAppVersionDialog(BuildContext context, Map<String, Color> theme, Color 
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Версия: 0.9.0', style: TextStyle(color: theme['secondary'])),
-          Text('Сборка: 13', style: TextStyle(color: theme['secondary'])),
-          Text('Дата: 02.11.2025', style: TextStyle(color: theme['secondary'])),
+          Text('Версия: 1.0.0', style: TextStyle(color: theme['secondary'])),
+          Text('Сборка: 20', style: TextStyle(color: theme['secondary'])),
+          Text('Дата: 21.02.2026', style: TextStyle(color: theme['secondary'])),
           const SizedBox(height: 16),
           Text('Последние изменения:', style: TextStyle(color: theme['text'], fontWeight: FontWeight.bold)),
           Text(
-            '• Добавлено "О приложении"\n• Добавлен функционал очистки кэша\n• Реализован экран Помощь',
+            '• Добавлено "Разделение на роли"\n• Добавлен список сообщений пользователя\n• Реализована логика чата',
             style: TextStyle(color: theme['secondary']),
           ),
         ],
